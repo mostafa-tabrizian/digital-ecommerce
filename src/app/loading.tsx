6fa647792b932ce2bc95566f65038209a6c7bff8
Mostafa-Code19 <tabrizian.codes@gmail.com>
2023-08-28 18:51:00 +0330

    add loadingBar at top

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,17 +1,46 @@
-const LoadingHome = () => {
-   return (
-      <div className='mx-6 my-16 md:mx-auto animate-pulse max-w-screen-md space-y-4'>
-         <div className='w-full h-40 bg-gray-200 rounded'></div>
-         <div className='w-full h-20 bg-gray-200 rounded'></div>
-         <div className='w-full h-10 bg-gray-200 rounded'></div>
+'use client'
 
-         <div className='grid grid-cols-2'>
-            <div className='w-45 h-40 bg-gray-200 mr-3 mb-3 rounded'></div>
-            <div className='w-45 h-40 bg-gray-200 mb-3 rounded'></div>
-            <div className='w-45 h-40 bg-gray-200 mr-3 rounded'></div>
-            <div className='w-45 h-40 bg-gray-200 rounded'></div>
+import LoadingBar from 'react-top-loading-bar'
+import { useEffect, useState } from 'react'
+
+const LoadingHome = () => {
+   const [progress, setProgress] = useState(0)
+
+   useEffect(() => {
+      let innerProgress = 0
+
+      const interval = setInterval(() => {
+         if (innerProgress >= 100) clearInterval(interval)
+
+         innerProgress += 2
+         setProgress((prev) => prev + 2)
+      }, 100)
+   }, [])
+
+   return (
+      <>
+         <LoadingBar
+            color='#2495ff'
+            height={5}
+            progress={progress}
+            waitingTime={400}
+            onLoaderFinished={() => {
+               setProgress(0)
+            }}
+         />
+         <div className='mx-6 my-16 md:mx-auto animate-pulse max-w-screen-md space-y-4'>
+            <div className='w-full h-40 bg-gray-200 rounded'></div>
+            <div className='w-full h-20 bg-gray-200 rounded'></div>
+            <div className='w-full h-10 bg-gray-200 rounded'></div>
+
+            <div className='grid grid-cols-2'>
+               <div className='w-45 h-40 bg-gray-200 mr-3 mb-3 rounded'></div>
+               <div className='w-45 h-40 bg-gray-200 mb-3 rounded'></div>
+               <div className='w-45 h-40 bg-gray-200 mr-3 rounded'></div>
+               <div className='w-45 h-40 bg-gray-200 rounded'></div>
+            </div>
          </div>
-      </div>
+      </>
    )
 }
 
