b148e41e39ab67c0704be39a590488fc16e307f2
Mostafa-Code19 <tabrizian.codes@gmail.com>
2023-09-03 11:37:07 +0330

    half space in persian validation rules

--- src/formik/schema/rules.tsx
+++ src/formik/schema/rules.tsx
@@ -1,4 +1,4 @@
-export const persian = /^[آ-ی ء چ . ، ! ؟ 1-9 ۱-۹ re \r\n  ? ! _ - ' " ; : , ؛ : ( ) ژ 0 ۰]+$/
+export const persian = /^[آ-ی ء چ . ، ! ؟ 1-9 ۱-۹ re \r\n /\u200c/g ? ! _ - ' " ; : , ؛ : ( ) ژ 0 ۰]+$/
 
 export const password = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[a-zA-Z]).{5,}$/
 
